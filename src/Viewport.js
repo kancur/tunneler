b88b1660df65fed68fa565e1824213519a6028b0
Peter Smid <petosmid@gmail.com>
2021-12-10 09:47:56 +0100

    clean up commented code

--- src/Viewport.js
+++ src/Viewport.js
@@ -14,17 +14,9 @@ export default class Viewport {
   update(offsetX, offsetY) {
     this.offsetX = offsetX;
     this.offsetY = offsetY;
-    //this.view = [];
-/*     for (let y = offsetY; y < this.height + offsetY; y++) {
-      for (let x = offsetX; x < this.width + offsetX; x++) {
-        const tile = this.gameMap.getTile(x, y);
-        this.view.push(tile);
-      }
-    } */
   }
 
   getTile(x, y) {
     return this.gameMap.getTile(x + this.offsetX, y + this.offsetY);
-    //return this.view[y * this.width + x];
   }
 }
