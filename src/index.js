2025818457509f9270a5ab6b0ae05cff2b55ce1b
Peter Smid <petosmid@gmail.com>
2021-12-10 14:26:30 +0100

    clean up

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
+import ConnectionHandler from "./ConnectionHandler";
 import Game from "./Game";
-import GameMap from "./map/GameMap";
-import Render from "./render/Render";
-import Viewport from "./Viewport";
-
-const gameMap = new GameMap();
-const viewport = new Viewport(gameMap);
 
 const game = new Game();
 
-
+const connectionHandler = new ConnectionHandler();
