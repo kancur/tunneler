b15c9cf18ce3e15e2506b55bb5446ebae3a10f7d
Peter Smid <petosmid@gmail.com>
2021-12-10 09:48:14 +0100

    remove unused code

--- src/index.js
+++ src/index.js
@@ -8,44 +8,4 @@ const viewport = new Viewport(gameMap);
 
 const game = new Game();
 
-const pressedKeys = {
-  down: false,
-  up: false,
-  left: false,
-  right: false,
-};
-
-
-/* document.addEventListener("keydown", ({ key }) => {
-  if (key === "ArrowUp") {
-    pressedKeys.up = true;
-  }
-  if (key === "ArrowDown") {
-    pressedKeys.down = true;
-  }
-  if (key === "ArrowRight") {
-    pressedKeys.right = true;
-  }
-  if (key === "ArrowLeft") {
-    pressedKeys.left = true;
-  }
-});
-
-document.addEventListener("keyup", ({ key }) => {
-  if (key === "ArrowUp") {
-    pressedKeys.up = false;
-  }
-  if (key === "ArrowDown") {
-    pressedKeys.down = false;
-  }
-  if (key === "ArrowRight") {
-    pressedKeys.right = false;
-  }
-  if (key === "ArrowLeft") {
-    pressedKeys.left = false;
-  }
-}); */
-
-//x += 1;
-
 
