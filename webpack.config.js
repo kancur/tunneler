9f5c45e330467773df0880c4dfec10ba432d03dc
Peter Smid <petosmid@gmail.com>
2021-12-10 17:03:26 +0100

    update config

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,15 @@
 const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  target: 'web',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: "auto",
+    //publicPath: "auto",
   },
   devtool: "source-map",
   devServer: {
@@ -15,18 +18,30 @@ module.exports = {
     },
     compress: true,
     port: 3000,
-    open: true,
+    hot: true,
+    //liveReload: true,
+    watchFiles: ['src/index.html']
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin(),
+  ],
   module: {
     rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(html)$/,
+        use: ["html-loader"],
+      },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
     ],
-  },
+  }
 };
